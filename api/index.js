cab059fada1bf8241abc8a229f4d4d05c97a53e4
Ajay Verma <ajayverma917034@gmail.com>
2023-10-19 13:02:11 +0530

    change to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import dotenv from "dotenv"
 const app = express()
 
-
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to mongodb')
+}).catch((error) => {
+    console.log(error)
+})
 
 app.listen(3000, ()=>{
     console.log("Server is listening on port 3000")
