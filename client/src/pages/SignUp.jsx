7c334975fcd977f6cfa8123411d8a50fdc746096
Ajay Verma <ajayverma917034@gmail.com>
2023-10-22 23:03:05 +0530

    sign up ui design

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
 const SignUp = () => {
   return (
-    <div>signup</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4 '>
+        <input type="text" placeholder='Username' className='border p-3 rounder-lg' id='username'/>
+        <input type="email" placeholder='Email' className='border p-3 rounder-lg' id='email'/>
+        <input type="password" placeholder='Password' className='border p-3 rounder-lg' id='password'/>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+      </form>
+      <div className='flex gap-2 mt-4'>
+        <p>Have an account?</p>
+        <Link to={'/sign-in'}> <span className='text-blue-700'>Sign in</span></Link>
+      </div>
+    </div>
   )
 }
 
