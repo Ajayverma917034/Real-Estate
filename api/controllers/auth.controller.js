466c632535d246de6d08a1e59aa95f80e5d85d85
Ajay Verma <ajayverma917034@gmail.com>
2023-10-23 00:00:01 +0530

    create sign api controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.modal.js";
 import bcryptjs from "bcryptjs"
+import { ErrorHandler } from "../utils/Error.js";
+import jwt from "jsonwebtoken"
 export const signup = async (req, res, next)=>{
     
     const {username, email, password} = req.body;
@@ -13,3 +15,23 @@ export const signup = async (req, res, next)=>{
         next(error);
     }
 }
+
+export const signin = async (req, res, next)=>{
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(ErrorHandler(404, 'User not found!'))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword)
+            return next(ErrorHandler(401, 'Wrong Credentials'))
+        
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        const {password: pass, ...rest} = validUser._doc;
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+    } catch (err) {
+        next(err) 
+    }
+} 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,19 +1,60 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useState } from 'react'
+import {Link, useNavigate} from "react-router-dom"
 const SignUp = () => {
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const handleChange = (e)=>{
+    setFormData({
+      ...formData, [e.target.id]: e.target.value,
+    })
+  }
+  const handleOnsubmit =async(e) =>{
+    e.preventDefault();
+    try {
+        setLoading(true)
+        const res = await fetch('/api/auth/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData),
+        });
+    
+        const data = await res.json();
+        if(data.success === false){
+          setError(data.message)
+          setLoading(false)
+          return;
+        }
+        setLoading(false)
+        setError(null)
+        navigate('/sign-in')
+      
+    } catch (error) {
+      setLoading(false)
+      setError(error.message)
+      console.log("hello")
+      
+    }
+    // console.log(data)
+  }
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4 '>
-        <input type="text" placeholder='Username' className='border p-3 rounder-lg' id='username'/>
-        <input type="email" placeholder='Email' className='border p-3 rounder-lg' id='email'/>
-        <input type="password" placeholder='Password' className='border p-3 rounder-lg' id='password'/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
+      <form className='flex flex-col gap-4 ' onSubmit={handleOnsubmit}>
+        <input type="text" placeholder='Username' className='border p-3 rounder-lg' id='username' onChange={handleChange}/>
+        <input type="email" placeholder='Email' className='border p-3 rounder-lg' id='email' onChange={handleChange}/>
+        <input type="password" placeholder='Password' className='border p-3 rounder-lg' id='password' onChange={handleChange}/>
+        <button disabled = {loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80' onClick={handleOnsubmit}>{loading ? 'Loading...' : 'Sign up' }</button>
       </form>
       <div className='flex gap-2 mt-4'>
         <p>Have an account?</p>
         <Link to={'/sign-in'}> <span className='text-blue-700'>Sign in</span></Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
