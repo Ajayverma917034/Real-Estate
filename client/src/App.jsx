00ced0ac6e8402a76bb05c6361016225a65f2be4
Ajay Verma <ajayverma917034@gmail.com>
2023-10-25 00:33:22 +0530

    update header and make protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,21 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import Home from './pages/Home'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 const App = () => {
-  console.log("Hello")
 
   return (
   <BrowserRouter>
     <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
+
+      <Route element={<PrivateRoute/>}>
+        <Route path='/profile' element={<Profile/>}/>
+      </Route>
+
       <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
-      <Route path='pofile/' element={<Profile/>}/>
       <Route path='/about' element={<About/>}/>
     </Routes>
   </BrowserRouter>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import {useSelector} from "react-redux"
 const Header = () => {
+    const {currentUser} = useSelector(state => state.user)
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -23,8 +25,13 @@ const Header = () => {
             <Link to='/about'>
                 <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
             </Link>
-            <Link to='/sign-in'>
-                <li className=' text-slate-700 hover:underline'>Sign In</li>
+            <Link to='/profile'>
+                {
+                    currentUser ? (
+                    <img src={currentUser.avatar} alt="profile" className='rounded-full h-8 w-8 object-cover '/>
+                    ):
+                    <li className=' text-slate-700 hover:underline'>Sign In</li>
+                }
             </Link>
 
         </ul>
